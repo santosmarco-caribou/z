f1ef470b3486c4ceb9fd5373ca567cbc9f5a18d2
santosmarco <eu@marco.rio.br>
2022-09-06 17:39:44 -0300

    Add `WithQuestionMarks`

--- src/utils.ts
+++ src/utils.ts
@@ -12,10 +12,19 @@ export namespace ZUtils {
     Options['flat'] extends true ? 'flat' : 'deep'
   >
 
+  type OptionalKeys<T extends O.Object> = {
+    [K in keyof T]: undefined extends T[K] ? K : never
+  }[keyof T]
+  type RequiredKeys<T extends O.Object> = {
+    [K in keyof T]: undefined extends T[K] ? never : K
+  }[keyof T]
+
   export type RequiredDeep<T extends O.Object> = O.Required<T, PropertyKey, 'deep'>
   export type MergeDeep<A extends O.Object, B extends O.Object> = O.Merge<A, B, 'deep'>
 
   export type OmitInternals<T extends O.Object> = Omit<T, `${'$_' | '_'}${string}`>
+
+  export type WithQuestionMarks<T extends O.Object> = Pick<T, RequiredKeys<T>> & Partial<Pick<T, OptionalKeys<T>>>
 
   export type MapToZOutput<T> = Simplify<{ [K in keyof T]: T[K] extends AnyZ ? _ZOutput<T[K]> : never }>
   export type MapToZInput<T> = Simplify<{ [K in keyof T]: T[K] extends AnyZ ? _ZInput<T[K]> : never }>

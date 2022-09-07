f841ebefba4a7a8ffb21c70344cf8c81abc7b48d
santosmarco <eu@marco.rio.br>
2022-09-06 23:07:34 -0300

    Improve ZUndefined specs

--- src/z/undefined.spec.ts
+++ src/z/undefined.spec.ts
@@ -1,42 +1,26 @@
+import { ZSpecUtils } from '../utils'
 import { ZUndefined } from './z'
 
 describe('ZUndefined', () => {
-  it("should have a hint of 'undefined'", () => {
-    const z = ZUndefined.create()
-    expect(z.hint).toBe('undefined')
-  })
-
-  /* ---------------------------------------------------------------------------------------------------------------- */
-
-  it('should parse `undefined`', () => {
-    const z = ZUndefined.create()
-    expect(z.parse(undefined)).toBe(undefined)
-  })
-
-  /* ---------------------------------------------------------------------------------------------------------------- */
-
-  it('should not parse `null`', () => {
-    const z = ZUndefined.create()
-    expect(() => z.parse(null)).toThrowError('"value" is not allowed')
-  })
-
-  it('should not parse a non-zero number', () => {
-    const z = ZUndefined.create()
-    expect(() => z.parse(1)).toThrowError('"value" is not allowed')
-  })
-
-  it('should not parse `0`', () => {
-    const z = ZUndefined.create()
-    expect(() => z.parse(0)).toThrowError('"value" is not allowed')
-  })
-
-  it('should not parse a string', () => {
-    const z = ZUndefined.create()
-    expect(() => z.parse('test')).toThrowError('"value" is not allowed')
-  })
-
-  it('should not parse an empty string', () => {
-    const z = ZUndefined.create()
-    expect(() => z.parse('')).toThrowError('"value" is not allowed')
+  ZSpecUtils.buildTypeSpec({
+    type: ZUndefined,
+    typeName: 'ZUndefined',
+    typeHint: 'undefined',
+    shouldParse: [undefined],
+    shouldNotParse: [
+      [null, '"value" is not allowed'],
+      [NaN, '"value" is not allowed'],
+      [-1, '"value" is not allowed'],
+      [0, '"value" is not allowed'],
+      [1, '"value" is not allowed'],
+      ['', '"value" is not allowed'],
+      ['test', '"value" is not allowed'],
+      [[], '"value" is not allowed'],
+      [[-1, 0, 1], '"value" is not allowed'],
+      [['', 'test'], '"value" is not allowed'],
+      [BigInt(-1), '"value" is not allowed'],
+      [BigInt(0), '"value" is not allowed'],
+      [BigInt(1), '"value" is not allowed'],
+    ],
   })
 })

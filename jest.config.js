1a2ef4202d0c43a3a8d5e1d28a718cd74f93eaac
santosmarco <eu@marco.rio.br>
2022-09-07 05:13:57 -0300

    Add test reporter

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['node_modules/', 'dist/'],
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: 'Test Report',
+      },
+    ],
+  ],
 }

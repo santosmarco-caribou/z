a166c53f337306e32d1c7bc9594ade472080f4cf
santosmarco <eu@marco.rio.br>
2022-09-06 21:44:25 -0300

    Improve ZAny specs

--- src/z/any.spec.ts
+++ src/z/any.spec.ts
@@ -6,25 +6,7 @@ describe('ZAny', () => {
     expect(z.hint).toBe('any')
   })
 
-  it('should parse a string', () => {
-    const z = ZAny.create()
-    expect(z.parse('test')).toBe('test')
-  })
-
-  it('should parse an empty string', () => {
-    const z = ZAny.create()
-    expect(z.parse('')).toBe('')
-  })
-
-  it('should parse a number', () => {
-    const z = ZAny.create()
-    expect(z.parse(1)).toBe(1)
-  })
-
-  it('should parse `NaN`', () => {
-    const z = ZAny.create()
-    expect(z.parse(NaN)).toBe(NaN)
-  })
+  /* ---------------------------------------------------------------------------------------------------------------- */
 
   it('should parse `undefined`', () => {
     const z = ZAny.create()
@@ -34,5 +16,25 @@ describe('ZAny', () => {
   it('should parse `null`', () => {
     const z = ZAny.create()
     expect(z.parse(null)).toBe(null)
+  })
+
+  it('should parse a non-zero number', () => {
+    const z = ZAny.create()
+    expect(z.parse(1)).toBe(1)
+  })
+
+  it('should parse `0`', () => {
+    const z = ZAny.create()
+    expect(z.parse(0)).toBe(0)
+  })
+
+  it('should parse a string', () => {
+    const z = ZAny.create()
+    expect(z.parse('test')).toBe('test')
+  })
+
+  it('should parse an empty string', () => {
+    const z = ZAny.create()
+    expect(z.parse('')).toBe('')
   })
 })

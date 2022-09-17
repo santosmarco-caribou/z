75d9e6ac9e050cc683c69a87c4686dfb71cfad65
santosmarco <eu@marco.rio.br>
2022-09-17 15:40:01 -0300

    Improve ZOpenApi

--- src/manifest/openapi.ts
+++ src/manifest/openapi.ts
@@ -1,15 +1,37 @@
-import j2s, { type SwaggerSchema } from 'joi-to-swagger'
+import j2s from 'joi-to-swagger'
+import { includes } from 'lodash'
+import type { SchemaObject } from 'openapi3-ts'
+import type { U } from 'ts-toolbelt'
 import type { Merge } from 'type-fest'
 
-import type { _ZOutput, BaseZ, ZDef, ZManifestObject } from '../_internals'
+import type { _ZOutput, AnyZManifestObject, BaseZ, ZDef, ZManifestObject } from '../_internals'
 import { mergeSafe } from '../utils'
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                      ZOpenApi                                                      */
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+const OPEN_API_PROPERTIES: U.ListOf<Extract<keyof AnyZManifestObject, keyof SchemaObject>> = [
+  'title',
+  'description',
+  'type',
+  'format',
+  'default',
+  'examples',
+  'deprecated',
+  'minimum',
+  'maximum',
+  'multipleOf',
+]
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ZOpenApi<Def extends ZDef> extends BaseZ<Def> {}
 export class ZOpenApi<Def extends ZDef> {
-  toOpenApi(): Merge<SwaggerSchema, ZManifestObject<_ZOutput<Def>>> {
+  toOpenApi(): Merge<ZManifestObject<_ZOutput<Def>>, SchemaObject> {
     return mergeSafe(
-      Object.fromEntries(Object.entries(this.$_manifest).map(([k, v]) => [`x-${k}`, v])),
+      Object.fromEntries(
+        Object.entries(this.$_manifest).map(([k, v]) => (includes(OPEN_API_PROPERTIES, k) ? [k, v] : [`x-${k}`, v]))
+      ),
       j2s(this.$_schema).swagger
     )
   }

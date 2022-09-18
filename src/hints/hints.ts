2a83cbcb5451c56b7aaed1d4e20488d38ab46c49
santosmarco <eu@marco.rio.br>
2022-09-18 12:28:03 -0300

    Update docs

--- src/hints/hints.ts
+++ src/hints/hints.ts
@@ -19,8 +19,8 @@ export const colorizeZHint = (hint: string): string => {
       .replaceAll(/(.)(\??:) /g, `$1${chalk.magenta('$2')} `)
       // `|` in unions and `$` in intersections
       .replaceAll(/ (\||&) /g, ` ${chalk.magenta('$1')} `)
-      // `instanceof` keyword
-      .replaceAll(/(instanceof)/g, chalk.magenta('$1'))
+      // `instanceof` keyword and type
+      .replaceAll(/(instanceof) (.*)\b/g, `${chalk.magenta('$1')} ${chalk.cyan('$2')}`)
       // string literals
       .replaceAll(/'(\w*)'/g, chalk.yellow("'$1'"))
       // types
@@ -28,7 +28,7 @@ export const colorizeZHint = (hint: string): string => {
         /(null|undefined|string|number|boolean|true|false|symbol|any|never|unknown|void|bigint|Date|Record|Readonly|Map|Set)/g,
         chalk.cyan('$1')
       )
-      // format
+      // format strings
       .replaceAll(/(\(\$[^)]*\))/g, chalk.italic.gray('$1'))
   )
 }
